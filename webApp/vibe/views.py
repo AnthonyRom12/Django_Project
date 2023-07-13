be8335e283e2022403aeb53f463e8f702b776eca
AnthonyRom12 <106614976+AnthonyRom12@users.noreply.github.com>
2023-07-13 15:10:22 +0300

    fifth commit

--- webApp/vibe/views.py
+++ webApp/vibe/views.py
@@ -5,16 +5,28 @@ from django.shortcuts import render
 
 views = [
     {
-        'Name': 'Anton',
-        'Title': 'Programmer',
-        'Content': '',
-        'Date_Posted': 'July 10, 2023'
+        'Top_Channels': 'CS GO world',
+        'Title': 'CS GO',
+        'Content': 'News, channels'
+
     },
     {
-        'Name': 'Oleg',
-        'Title': 'Programmer',
-        'Content': '',
-        'Date_Posted': 'July 10, 2023'
+        'Top_Channels': 'The Witcher fans',
+        'Title': 'The Witcher',
+        'Content': 'News, channels'
+
+    },
+    {
+        'Top_Channels': 'Gangsters',
+        'Title': 'Grand Theft Auto 5',
+        'Content': 'News, channels'
+
+    },
+    {
+        'Top_Channels': 'Cowboy',
+        'Title': 'Red Dead Redemption 2',
+        'Content': 'News, channels'
+
     }
 ]
 
--- webApp/webApp/urls.py
+++ webApp/webApp/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+# for css
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vibe.urls')),
-
 ]
+
+urlpatterns += staticfiles_urlpatterns()
